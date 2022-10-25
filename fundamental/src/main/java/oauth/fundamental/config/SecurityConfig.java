cefab2f0c26c9d6dd824bf457f11f3a97c27dff5
Ji Seungmin <cswcsm02@gmail.com>
2022-10-25 21:09:24 +0900

    CustomAuthenticationEntryPoint 등록

---
+++ fundamental/src/main/java/oauth/fundamental/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package oauth.fundamental.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated();
+//        http.formLogin();
+//        http.httpBasic();
+        http.exceptionHandling().authenticationEntryPoint(new AuthenticationEntryPoint() {
+            @Override
+            public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+                System.out.println("customEntryPoint");
+            }
+        });
+        return http.build();
+    }
+
+
+}
