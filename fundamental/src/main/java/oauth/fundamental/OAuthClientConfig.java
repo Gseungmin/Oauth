a84b63a96b231f68079628bbf36acc528bd5933b
Ji Seungmin <cswcsm02@gmail.com>
2022-11-05 12:49:24 +0900

    디폴트 로그인 페이지 사용 설정

--- fundamental/src/main/java/oauth/fundamental/OAuthClientConfig.java
+++ fundamental/src/main/java/oauth/fundamental/OAuthClientConfig.java
@@ -1,6 +1,7 @@
 package oauth.fundamental;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,8 +12,11 @@ public class OAuthClientConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests(authRequest ->
-                authRequest.antMatchers("/loginPage").permitAll().anyRequest().authenticated());
-        http.oauth2Login(oauth2 -> oauth2.loginPage("/loginPage")); //로그인 페이지 설정
+                authRequest
+//                        .antMatchers("/loginPage").permitAll().
+                        .anyRequest().authenticated());
+//        http.oauth2Login(oauth2 -> oauth2.loginPage("/loginPage")); //로그인 페이지 설정
+        http.oauth2Login(Customizer.withDefaults()); //로그인 디폴트 설정
         return http.build();
     }
 }
