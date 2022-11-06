acc404a37c38e06daef6b6d1e287c7690dc0c020
Ji Seungmin <cswcsm02@gmail.com>
2022-11-06 11:05:05 +0900

    FEAT: Authorization BaseUri Custom 구현

--- fundamental/src/main/java/oauth/fundamental/OAuthClientConfig.java
+++ fundamental/src/main/java/oauth/fundamental/OAuthClientConfig.java
@@ -1,7 +1,6 @@
 package oauth.fundamental;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,12 +10,10 @@ public class OAuthClientConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests(authRequest ->
-                authRequest
-//                        .antMatchers("/loginPage").permitAll().
-                        .anyRequest().authenticated());
-//        http.oauth2Login(oauth2 -> oauth2.loginPage("/loginPage")); //로그인 페이지 설정
-        http.oauth2Login(Customizer.withDefaults()); //로그인 디폴트 설정
+        http.authorizeRequests(request -> request.anyRequest().authenticated());
+        http.oauth2Login(oauth2 -> oauth2.loginPage("/login")
+                .authorizationEndpoint(authorizationEndpointConfig ->
+                        authorizationEndpointConfig.baseUri("/oauth2/v1/authorization")));
         return http.build();
     }
 }
