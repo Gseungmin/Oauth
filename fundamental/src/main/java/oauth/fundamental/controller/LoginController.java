c05feb680996d5226d7e7b195d609e39daac07c7
Ji Seungmin <cswcsm02@gmail.com>
2022-11-06 11:28:30 +0900

    FEAT: 로그인 페이지 구현 및 연동

--- fundamental/src/main/java/oauth/fundamental/controller/LoginController.java
+++ fundamental/src/main/java/oauth/fundamental/controller/LoginController.java
@@ -1,14 +1,15 @@
 package oauth.fundamental.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class LoginController {
 
-    @GetMapping("/loginPage")
-    public String loginPage() {
-        return "loginPage";
+    @GetMapping("/login")
+    public String login() {
+        return "login";
     }
 
 }
