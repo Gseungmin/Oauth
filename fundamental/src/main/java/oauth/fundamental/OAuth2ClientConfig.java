008237952671c205be65c6efc874e936d9114787
Ji Seungmin <cswcsm02@gmail.com>
2022-11-06 20:34:57 +0900

    FEAT: OAuth2.0 Social Login 기본 기능 및 webSecurity 설정 커스터마이징 기능 구현

---
+++ fundamental/src/main/java/oauth/fundamental/OAuth2ClientConfig.java
@@ -0,0 +1,34 @@
+package oauth.fundamental;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class OAuth2ClientConfig {
+
+    /**
+     * webSecurity 설정을 변경할 수 있도록 Customizer 인터페이스가 제공
+     * */
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .antMatchers("/static/js/**");
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests(request -> request.
+                antMatchers("/").permitAll().
+                anyRequest().authenticated());
+        http.oauth2Login(Customizer.withDefaults());
+        http.logout().logoutSuccessUrl("/");
+        return http.build();
+    }
+
+
+
+}
